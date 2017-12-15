36628b5b2794347f093e5e419b121facfc95901b
DrobotAlena <ukiro@mail.ru>
2017-12-15 12:10:12 +0700

    обновлен StepRules

--- AwesomeChess/src/main/java/ru/nsu/fit/g14203/dreamteam/awesomechess/field/StepRules.java
+++ AwesomeChess/src/main/java/ru/nsu/fit/g14203/dreamteam/awesomechess/field/StepRules.java
@@ -5,6 +5,8 @@
  */
 package ru.nsu.fit.g14203.dreamteam.awesomechess.field;
 
+import com.sun.beans.util.Cache;
+
 /**
  *
  * @author phoenix
@@ -16,7 +18,59 @@ public class StepRules {
     
     //Тип фигуры (По порядку: король, ферзь, ладья, конь, слон, пешка)
     public static enum FigureType { KING, QUEEN, ROOK, KNIGHT, BISHOP, PAWN}
-    
+
+    public boolean canFigureGo(FigureType figureType, FieldCoord figureCoord, FieldCoord targetCoord){
+        if (figureCoord.equals(targetCoord)){
+            return false;
+        }
+        else if (figureType == FigureType.KING){
+            return canKingGo(figureCoord, targetCoord);
+        }
+        else if (figureType == FigureType.QUEEN){
+            return canQueenGo(figureCoord, targetCoord);
+        }
+        else if (figureType == FigureType.ROOK){
+            return canRookGo(figureCoord, targetCoord);
+        }
+        else if (figureType == FigureType.KNIGHT){
+            return canKnightGo(figureCoord, targetCoord);
+        }
+        else if (figureType == FigureType.BISHOP){
+            return canBishopGo(figureCoord, targetCoord);
+        }
+        else if (figureType == FigureType.PAWN){
+            return canPawnGo(figureCoord, targetCoord);
+        }
+        return false;
+    }
+
+    private boolean canKingGo(FieldCoord figureCoord, FieldCoord targetCoord){
+
+    }
+
+    private boolean canQueenGo(FieldCoord figureCoord, FieldCoord targetCoord){
+        if(figureCoord.X == targetCoord.X){
+
+        }
+    }
+
+    private boolean canRookGo(FieldCoord figureCoord, FieldCoord targetCoord){
+
+    }
+
+    private boolean canKnightGo(FieldCoord figureCoord, FieldCoord targetCoord){
+
+    }
+
+    private boolean canBishopGo(FieldCoord figureCoord, FieldCoord targetCoord){
+
+    }
+
+    private boolean canPawnGo(FieldCoord figureCoord, FieldCoord targetCoord){
+
+    }
+
+
     /*//Направление хода с ограничением. Например, пешки ходят только на 1-2 клетки
     public static class StepDirectionWithLimit {
         public final FigureType direction;
