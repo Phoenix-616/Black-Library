5d2c0fe9fc15584b01045323617a445e92580076
DrobotAlena <ukiro@mail.ru>
2017-12-15 12:47:55 +0700

    Обновлены функции хода ферзя, слона и ладьи в классе StepRules

--- AwesomeChess/src/main/java/ru/nsu/fit/g14203/dreamteam/awesomechess/field/StepRules.java
+++ AwesomeChess/src/main/java/ru/nsu/fit/g14203/dreamteam/awesomechess/field/StepRules.java
@@ -19,54 +19,149 @@ public class StepRules {
     //Тип фигуры (По порядку: король, ферзь, ладья, конь, слон, пешка)
     public static enum FigureType { KING, QUEEN, ROOK, KNIGHT, BISHOP, PAWN}
 
-    public boolean canFigureGo(FigureType figureType, FieldCoord figureCoord, FieldCoord targetCoord){
-        if (figureCoord.equals(targetCoord)){
-            return false;
-        }
-        else if (figureType == FigureType.KING){
-            return canKingGo(figureCoord, targetCoord);
+    public static boolean canFigureGo(FigureType figureType, FieldCoord figureCoord, FieldCoord targetCoord, Model model){
+        if (figureType == FigureType.KING){
+            return canKingGo(figureCoord, targetCoord,model);
         }
         else if (figureType == FigureType.QUEEN){
-            return canQueenGo(figureCoord, targetCoord);
+            return canQueenGo(figureCoord, targetCoord, model);
         }
         else if (figureType == FigureType.ROOK){
-            return canRookGo(figureCoord, targetCoord);
+            return canRookGo(figureCoord, targetCoord, model);
         }
         else if (figureType == FigureType.KNIGHT){
-            return canKnightGo(figureCoord, targetCoord);
+            return canKnightGo(figureCoord, targetCoord, model);
         }
         else if (figureType == FigureType.BISHOP){
-            return canBishopGo(figureCoord, targetCoord);
+            return canBishopGo(figureCoord, targetCoord, model);
         }
         else if (figureType == FigureType.PAWN){
-            return canPawnGo(figureCoord, targetCoord);
+            return canPawnGo(figureCoord, targetCoord, model);
         }
         return false;
     }
 
-    private boolean canKingGo(FieldCoord figureCoord, FieldCoord targetCoord){
+    private static boolean canKingGo(FieldCoord figureCoord, FieldCoord targetCoord, Model model){
 
     }
 
-    private boolean canQueenGo(FieldCoord figureCoord, FieldCoord targetCoord){
-        if(figureCoord.X == targetCoord.X){
+    private static boolean canQueenGo(FieldCoord figureCoord, FieldCoord targetCoord, Model model){
+        if(canRookGo(figureCoord, targetCoord, model)){
+            return true;
+        }
+        else return canBishopGo(figureCoord, targetCoord, model);
+    }
 
+    private static boolean canRookGo(FieldCoord figureCoord, FieldCoord targetCoord, Model model){
+        int y=0, x=0;
+        if(figureCoord.X == targetCoord.X){
+            if(figureCoord.Y < targetCoord.Y){
+                y = figureCoord.Y+1;
+                while(y < targetCoord.Y){
+                    if( !model.cellIsEmpty(new FieldCoord(figureCoord.X, y))){
+                        return false;
+                    }
+                    y++;
+                }
+                return true;
+            }
+            if(figureCoord.Y > targetCoord.Y){
+                y = figureCoord.Y-1;
+                while(y > targetCoord.Y){
+                    if( !model.cellIsEmpty(new FieldCoord(figureCoord.X, y))){
+                        return false;
+                    }
+                    y--;
+                }
+                return true;
+            }
+        }
+        else if(figureCoord.Y == targetCoord.Y){
+            if(figureCoord.X < targetCoord.X){
+                x = figureCoord.X+1;
+                while(x < targetCoord.X){
+                    if( !model.cellIsEmpty(new FieldCoord(x, figureCoord.Y))){
+                        return false;
+                    }
+                    x++;
+                }
+                return true;
+            }
+            if(figureCoord.X > targetCoord.X){
+                x = figureCoord.X-1;
+                while(x > targetCoord.X){
+                    if( !model.cellIsEmpty(new FieldCoord(x, figureCoord.Y))){
+                        return false;
+                    }
+                    x--;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean canKnightGo(FieldCoord figureCoord, FieldCoord targetCoord, Model model){
+
+    }
+
+    private static boolean canBishopGo(FieldCoord figureCoord, FieldCoord targetCoord, Model model){
+        int x=0, y=0;
+        if(figureCoord.Y < targetCoord.Y){
+            if(figureCoord.X < targetCoord.X){
+                x=figureCoord.X+1;
+                y=figureCoord.Y+1;
+                while (y < targetCoord.Y && x < targetCoord.X){
+                    if(!model.cellIsEmpty(new FieldCoord(x,y))){
+                        return false;
+                    }
+                    x++;
+                    y++;
+                }
+                return true;
+            }
+            else{
+                x=figureCoord.X-1;
+                y=figureCoord.Y+1;
+                while (y < targetCoord.Y && x > targetCoord.X){
+                    if(!model.cellIsEmpty(new FieldCoord(x,y))){
+                        return false;
+                    }
+                    x--;
+                    y++;
+                }
+                return true;
+            }
+        }
+        else{
+            if(figureCoord.X < targetCoord.X){
+                x=figureCoord.X+1;
+                y=figureCoord.Y-1;
+                while (y > targetCoord.Y && x < targetCoord.X){
+                    if(!model.cellIsEmpty(new FieldCoord(x,y))){
+                        return false;
+                    }
+                    x++;
+                    y--;
+                }
+                return true;
+            }
+            else{
+                x=figureCoord.X-1;
+                y=figureCoord.Y-1;
+                while (y > targetCoord.Y && x > targetCoord.X){
+                    if(!model.cellIsEmpty(new FieldCoord(x,y))){
+                        return false;
+                    }
+                    x--;
+                    y--;
+                }
+                return true;
+            }
         }
     }
 
-    private boolean canRookGo(FieldCoord figureCoord, FieldCoord targetCoord){
-
-    }
-
-    private boolean canKnightGo(FieldCoord figureCoord, FieldCoord targetCoord){
-
-    }
-
-    private boolean canBishopGo(FieldCoord figureCoord, FieldCoord targetCoord){
-
-    }
-
-    private boolean canPawnGo(FieldCoord figureCoord, FieldCoord targetCoord){
+    private static boolean canPawnGo(FieldCoord figureCoord, FieldCoord targetCoord, Model model){
 
     }
 
