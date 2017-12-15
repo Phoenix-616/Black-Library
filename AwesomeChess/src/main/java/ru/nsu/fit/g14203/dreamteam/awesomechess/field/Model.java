93a325a786f5c09005e62528eddc79affa949c0a
Ekaterina Trofimova <onesen@yandex.ru>
2017-12-15 10:58:02 +0700

    Model changes

---
+++ AwesomeChess/src/main/java/ru/nsu/fit/g14203/dreamteam/awesomechess/field/Model.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.nsu.fit.g14203.dreamteam.awesomechess.field;
+
+/**
+ *
+ * @author ekaterina
+ */
+public class Model {
+    
+}
